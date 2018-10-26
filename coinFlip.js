57b864a56e40bcb81472ddbe526a14ebd6590ab4
danejackins <dane.jackins@gmail.com>
2018-10-25 18:33:51 -0600

    init commit

---
+++ coinFlip.js
@@ -0,0 +1,47 @@
+var headsCount = 0;
+var tailsCount = 0;
+var wins = 0;
+var losses = 0;
+
+function coinFlip(result) {
+
+  var randomNumber = Math.floor(Math.random() * 2);
+
+  if (randomNumber === 0) {
+    $("#coin-image").html(
+      "<img src='http://random-ize.com/coin-flip/us-quater/us-quarter-front.jpg' />"
+    );
+  }
+
+  else {
+    $("#coin-image").html(
+      "<img src='http://random-ize.com/coin-flip/us-quarter/us-quarter-back.jpg' />"
+    );
+  }
+
+  if (result === randomNumber) {
+    wins++;
+    $("#win-lose").html("<h2>Winner!</h2>");
+    $("#wins").html("<h3>" + wins + "</h3>");
+  }
+
+  else {
+    losses++;
+    $("#wins-lose").html("<h2>Loser!</h2>");
+    $("#losses").html("<h3>" + losses + "</h3>");
+  }
+}
+
+$("#heads").on("click", function() {
+  headsCount++;
+  $("#heads-chosen").text(headsCount);
+  $("#guess").html("<b>Heads</b>");
+  coinFlip(0);
+});
+
+$("#tails").on("click", function() {
+  tailsCount++;
+  $("#tails-chosen").text(tailsCount);
+  $("#guess").html("<b>Tails</b>");
+  coinFlip(1);
+});
